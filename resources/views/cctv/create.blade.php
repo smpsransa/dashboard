1d15c40386ba44a34894cbb96963ab1297690c3a
sandikodev <androxoss@hotmail.com>
2024-01-30 08:14:09 +0700

    cctv create, edit, delete action

--- resources/views/cctv/create.blade.php
+++ resources/views/cctv/create.blade.php
@@ -1,24 +1,22 @@
 @extends('layouts.app')
-
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-12">
                 <div class="card">
                     <div class="card-header" style="display: flex; justify-content: space-between;">
                         <a class="btn btn-success" href="{{ route('cctv.index') }}">Back</a>
-                        <span style="display: inline-flex; align-items: center;">{{ __('Edit CCTV') }}</span>
+                        <span style="display: inline-flex; align-items: center;">{{ __('Create CCTV') }}</span>
                     </div>
                     <div class="card-body">
-                        <form action="{{ route('cctv.update', $cctv->id) }}" method="POST" enctype="multipart/form-data">
+                        <form action="{{ route('cctv.store') }}" method="POST" enctype="multipart/form-data">
                             @csrf
-                            @method('PUT')
                             <div class="row">
                                 <div class="py-2 col-xs-12 col-sm-12 col-md-12">
                                     <div class="form-group">
                                         <strong>Inisial CCTV:</strong>
-                                        <input type="text" name="name" value="{{ $cctv->name }}"
-                                            class="form-control" placeholder="[lokasi]-[nama]">
+                                        <input type="text" name="name" class="form-control"
+                                            placeholder="[lokasi]-[nama]">
                                         @error('name')
                                             <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
                                         @enderror
@@ -28,7 +26,7 @@
                                     <div class="form-group">
                                         <strong>Link CCTV:</strong>
                                         <input type="text" name="address" class="form-control"
-                                            placeholder="http://example.com/channel/001/001" value="{{ $cctv->address }}">
+                                            placeholder="http://example.com/channel/001/001">
                                         @error('address')
                                             <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
                                         @enderror
--- resources/views/cctv/index.blade.php
+++ resources/views/cctv/index.blade.php
@@ -5,7 +5,8 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header" style="display: flex; justify-content: space-between;">{{ __('CCTV') }}
+                    <div class="card-header" style="display: flex; justify-content: space-between;">
+                        <span style="display: inline-flex; align-items: center;">{{ __('CCTV') }}</span>
                         <a class="btn btn-success" href="{{ route('cctv.create') }}">Registering CCTV</a>
                     </div>
                     <div class="card-body">
@@ -25,9 +26,17 @@
                                         <td>{{ $item->name }}</td>
                                         <td>{{ $item->address }}</td>
                                         <td>
+                                            <a id={{ $item->id }} class="btn btn-info"
+                                                href="{{ $item->address }}">view</a>
                                             <a id={{ $item->id }} class="btn btn-success"
-                                                href="route('cctv.edit')">edit</a>
-
+                                                href="{{ route('cctv.edit', $item->id) }}">edit</a>
+                                            <form action="{{ route('cctv.destroy', $item->id) }}" method="POST"
+                                                style="display:inline;">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-danger">delete</button>
+                                            </form>
+                                        </td>
                                     </tr>
                                 @endforeach
                             </tbody>
