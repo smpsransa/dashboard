d3b771804def8a3ce7dd0407fe915e43efdcda7e
sandikodev <androxoss@hotmail.com>
2024-01-30 13:04:19 +0700

    Unable to locate file in Vite manifest: resources/css/table.css

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/sass/app.scss',
+                'resources/css/table.css',
                 'resources/js/app.js',
             ],
             refresh: true,
