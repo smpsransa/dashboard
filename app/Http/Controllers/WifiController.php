754e40dd9a3d6148315174a1239145aff8a77194
sandikodev <androxoss@hotmail.com>
2024-02-01 06:36:03 +0700

    fix middleware for several pages

--- app/Http/Controllers/WifiController.php
+++ app/Http/Controllers/WifiController.php
@@ -7,10 +7,10 @@ use Illuminate\Http\Request;
 
 class WifiController extends Controller
 {
-    // public function __construct()
-    // {
-    //     $this->middleware('auth');
-    // }
+    public function __construct()
+    {
+        $this->middleware('auth')->except('index');
+    }
 
     public function index()
     {
--- resources/views/wifi/index.blade.php
+++ resources/views/wifi/index.blade.php
@@ -1,55 +1,58 @@
 @extends('layouts.app')
 @vite(['resources/css/table.css'])
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header" style="display: flex; justify-content: space-between;">
-                        <span style="display: inline-flex; align-items: center;">{{ __('Wifi') }}</span>
-                        <a class="btn btn-success" href="{{ route('wifi.create') }}">Registering Wifi</a>
-                    </div>
-                    <div class="card-body">
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>NO</th>
-                                    <th class="stretch">Ruang</th>
-                                    <th class="stretch">Devices</th>
-                                    <th class="stretch">SSID</th>
-                                    <th class="stretch">Network</th>
-                                    <th class="fit">Parent</th>
-                                    <th class="fit">Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($wifi as $item)
-                                    <tr>
-                                        <td>{{ $loop->index + 1 }}</td>
-                                        <td>{{ $item->ruang }}</td>
-                                        <td>{{ $item->devices }}</td>
-                                        <td>{{ $item->ssid }}</td>
-                                        <td>{{ $item->network }}</td>
-                                        <td>{{ $item->parent }}</td>
-                                        <td>
-                                            <a id={{ $item->id }} class="btn btn-info"
-                                                href="{{ route('wifi.show', $item->id) }}">view</a>
-                                            <a id={{ $item->id }} class="btn btn-success"
-                                                href="{{ route('wifi.edit', $item->id) }}">edit</a>
-                                            <form action="{{ route('wifi.destroy', $item->id) }}" method="POST"
-                                                style="display:inline;">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="btn btn-danger">delete</button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header" style="display: flex; justify-content: space-between;">
+                    <span style="display: inline-flex; align-items: center;">{{ __('Wifi') }}</span>
+                    @if (!Auth::guest())
+                    <a class="btn btn-success" href="{{ route('wifi.create') }}">Registering Wifi</a>
+                    @endif
+                </div>
+                <div class="card-body">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>NO</th>
+                                <th class="stretch">Ruang</th>
+                                <th class="stretch">Devices</th>
+                                <th class="stretch">SSID</th>
+                                <th class="stretch">Network</th>
+                                <th class="fit">Parent</th>
+                                @if (!Auth::guest())
+                                <th class="fit">Action</th>
+                                @endif
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($wifi as $item)
+                            <tr>
+                                <td>{{ $loop->index + 1 }}</td>
+                                <td>{{ $item->ruang }}</td>
+                                <td>{{ $item->devices }}</td>
+                                <td>{{ $item->ssid }}</td>
+                                <td>{{ $item->network }}</td>
+                                <td>{{ $item->parent }}</td>
+                                @if (!Auth::guest())
+                                <td>
+                                    <a id={{ $item->id }} class="btn btn-info" href="{{ route('wifi.show', $item->id) }}">view</a>
+                                    <a id={{ $item->id }} class="btn btn-success" href="{{ route('wifi.edit', $item->id) }}">edit</a>
+                                    <form action="{{ route('wifi.destroy', $item->id) }}" method="POST" style="display:inline;">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger">delete</button>
+                                    </form>
+                                </td>
+                                @endif
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
